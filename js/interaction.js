61effdd14ee2c615bd58a1fa13ca9388629bd160
Anderson <andersecng@gmail.com>
2017-06-22 18:36:27 -0400

    Implement cd and update generate layout

---
+++ js/interaction.js
@@ -0,0 +1,41 @@
+var openNav = false;
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function toggleNav() {
+    if(openNav){
+        document.getElementById("openbtn").style.color = "black";
+        document.getElementById("sidenav").style.marginLeft = "-250px";
+        document.getElementById("sidenav").style.boxShadow = "none";
+        document.getElementById("main").style.marginLeft = "0";
+        openNav = false;
+    }else{
+        document.getElementById("openbtn").style.color = "rgb(224, 224, 224)";
+        document.getElementById("sidenav").style.marginLeft = "0px";
+        document.getElementById("sidenav").style.boxShadow = "7px 0 28px rgba(0,0,0,0.25), 5px 0 28px rgba(0,0,0,0.30)";
+        document.getElementById("main").style.marginLeft = "250px";
+        openNav = true;
+    }
+};
+
+function openTerminal() {
+    document.getElementById("info").style.display = "none";
+    document.getElementById("history").innerHTML = "<span>Type 'help' to see list of commands</span><br/>";
+    document.getElementById("terminal").style.display = "block";
+    toggleNav();
+    focusOnTerminal();
+};
+
+function closeTerminal(){
+  document.getElementById("info").style.display = "block";
+  document.getElementById("terminal").style.display = "none";
+};
+
+
+function focusOnTerminal(){
+  document.getElementById("terminal-input").focus();
+};
+
+
+function updateScroll(){
+    var element = document.getElementById("text-box");
+    element.scrollTop = element.scrollHeight;
+};
--- js/terminal.js
+++ js/terminal.js
@@ -1,50 +1,89 @@
 var filesystem = {
-  "/" : {name: "/", files : ["about_me.txt", "resume.pdf"], directories : ["test"]},
-  "test" : {name: "test", files:[], directories:[]},
-  currentDir : "/"
+  "/" : {path: "/",
+        files : ["about_me.txt", "resume.pdf"],
+        directories : {"test" : {path: "/test", files:[], directories:[]},
+                       "test2" : {path: "/test2", files:["test2file"], directories:[]}}
+      },
+  currentDir : []
 };
 
-var history = [];
+var cmdHistory = [];
+var cmdIndex = -1;
 
 window.onload = function() {
   focusOnTerminal();
 };
 
-function focusOnTerminal(){
-  document.getElementById("terminal-input").focus();
-};
 
 function checkInput(e){
   var key = 'which' in e ? e.which : e.keyCode;
-  if(key == 13){
-    evaluateInput();
-    document.getElementById("terminal-input").focus();
+  switch (key) {
+    case 13:
+      evaluateInput();
+      document.getElementById("terminal-input").focus();
+      cmdIndex = cmdHistory.length - 1;
+      break;
+    case 38:
+      if(cmdIndex > -1){
+        document.getElementById("terminal-input").value = cmdHistory[cmdIndex];
+        cmdIndex--;
+      }
+      break;
+    case 40:
+      if (cmdIndex == cmdHistory.length - 1){
+        document.getElementById("terminal-input").value = "";
+      }else if(cmdIndex < cmdHistory.length){
+        cmdIndex++;
+        document.getElementById("terminal-input").value = cmdHistory[cmdIndex];
+      }
+      break;
   }
 };
 
 function evaluateInput(){
   var clear = false;
   var input = document.getElementById("terminal-input").value;
+  if (input.trim()!=""){
+    cmdHistory.push(input);
+  }
   var inputList = input.split(" ");
   var returnValue = "";
   var command = inputList[0];
   switch (command){
+    case "cd":
+      returnValue += cd(inputList, input);
+      break;
     case "ls":
-      returnValue += ls()  +"<br/>";
+      returnValue += ls();
       break;
     case "pwd":
-      returnValue += filesystem["currentDir"]  +"<br/>";
+      returnValue += pwd();
       break;
     case "cat":
-      returnValue += cat(inputList, input) +"<br/>";
+      returnValue += cat(inputList, input);
       break;
     case "clear":
       clear = true;
       break;
+    case "help":
+      returnValue += "Available commands<br/>"
+                  + "<span class='pink'>cat</span> [files] - print file content to console<br/> "
+                  + "<span class='pink'>cd</span> [directory] - change directory<br/> "
+                  + "<span class='pink'>clear</span> - clears console<br/> "
+                  + "<span class='pink'>ls</span> - list out content of current directory<br/>"
+                  + "<span class='pink'>pwd</span> - print working directory path<br/>"
+                  + "<span class='info'>If you'd prefer to view information without the Terminal, close the Terminal.</span>";
+
+      break;
+    case "mkdir":
+    case "sudo":
+      returnValue += input + ": Permission denied";
+      break;
     default:
       if (input.trim()!="")
-        returnValue += input+": command not found" +"<br/>";
+        returnValue += input+": command not found";
   }
+  if (returnValue.trim()!="") returnValue += "<br/>";
   if (clear) document.getElementById('history').innerHTML = "";
   else document.getElementById('history').innerHTML += "Anderson-Site:~ Visitor$ "
   + input + "<br />" + returnValue;
@@ -53,19 +92,47 @@ function evaluateInput(){
   updateScroll()
 };
 
+function cd(inputList, input){
+  if (inputList.length < 2) filesystem["currentDir"] = [];
+  else{
+    var directoryList = inputList[1].split("/");
+    var absolute = directoryList[0] != "" ? false : true;
+    var cleanedList = [];
+    for (i = 0; i < directoryList.length; i++){
+      if (directoryList[i].trim() != "") cleanedList.push(directoryList[i].trim());
+    }
+
+    var destination;
+    var newPath;
+    if(absolute){
+      destination = filesystem["/"]["directories"];
+      newPath = cleanedList;
+    }else{
+      destination = getCurrentDirectory()["directories"];
+      newPath = filesystem["currentDir"].concat(cleanedList);
+    }
+    for (i = 0; i < cleanedList.length; i++){
+      if(cleanedList[i] in destination) destination = destination[cleanedList[i]]["directories"];
+      else return input + ": " + "No such directory";
+    }
+    filesystem["currentDir"] = newPath;
+  }
+  return "";
+}
+
 function ls(){
   var value = ""
   var files = [];
   var file;
-  var currentDirectory = filesystem["currentDir"];
-  for (i = 0; i < filesystem[currentDirectory]["files"].length; i++){
-    file = filesystem[currentDirectory]["files"][i];
+  var currentDirectory = getCurrentDirectory();
+  for (i = 0; i < currentDirectory["files"].length; i++){
+    file = currentDirectory["files"][i];
     if(file == "resume.pdf") files.push("<a href=\"docs/resume.pdf\" id=\"resume\">resume.pdf</a>");
-    else files.push(filesystem[currentDirectory]["files"][i]);
+    else files.push(currentDirectory["files"][i]);
   }
 
-  for (i = 0; i < filesystem[currentDirectory]["directories"].length; i++){
-    files.push("<span class=\"directory\">" + filesystem[currentDirectory]["directories"][i] + "</span>");
+  for (i = 0; i < Object.keys(currentDirectory["directories"]).length; i++){
+    files.push("<span class=\"directory\">" + Object.keys(currentDirectory["directories"])[i] + "</span>");
   }
   files = files.sort(function(a, b){
     if (a > b) return 1;
@@ -81,6 +148,7 @@ function ls(){
 
 function cat(files, input){
   var value = ""
+  var currentDirectory = getCurrentDirectory();
   if (files.length > 1){
     for (i = 1; i < files.length; i++){
       if (files[i] == "about_me.txt"){
@@ -97,7 +165,7 @@ function cat(files, input){
 
       }else if (files[i] == ""){
         value += "Download my resume <a href=\"\">here</a>";
-      }else if (filesystem[filesystem["currentDir"]]["directories"].indexOf(files[i]) != -1){
+      }else if (currentDirectory["directories"][files[i]] !== "undefined"){
         value += input +": " + files[i] + " is a directory";
       }
       if(i != files.length - 1){
@@ -109,7 +177,16 @@ function cat(files, input){
   return value;
 }
 
-function updateScroll(){
-    var element = document.getElementById("text-box");
-    element.scrollTop = element.scrollHeight;
-};
+function pwd(){
+  if (filesystem["currentDir"].length == 0) return "/";
+  else return "/" + filesystem["currentDir"].join("/");
+}
+
+function getCurrentDirectory(){
+  var currentDir = filesystem["/"];
+  if (filesystem["currentDir"].length == 0) return currentDir;
+  for (i = 0; i < filesystem["currentDir"].length; i++){
+    currentDir = currentDir["directories"][filesystem["currentDir"][i]];
+  }
+  return currentDir;
+}
